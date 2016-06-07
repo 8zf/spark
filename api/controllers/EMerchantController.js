ce51c540006e9fc0e5f77351645ef06c66eedc20
Yizhe <yuanyizhe@gmail.com>
2016-06-07 15:04:03 +0800

    add eMerchant

---
+++ api/controllers/EMerchantController.js
@@ -0,0 +1,11 @@
+/**
+ * EMerchantController
+ *
+ * @description :: Server-side logic for managing Emerchants
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
