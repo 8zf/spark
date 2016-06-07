3decd08ce0acef884760d71a4c0f85400fe26955
zhangfeng <zhangfeng2333@gmail.com>
2016-06-07 19:56:07 +0800

    Merge

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -26,6 +26,76 @@ module.exports = {
 
   sign (req, res) {
 
+  },
+
+  test (req, res) {
+    var order = {
+      "order": {
+        "sender": {
+          "name": "袁毅哲",
+          "phone": "911110",
+          "address": "住在家里",
+          "createdAt": "2016-06-06T05:44:44.148Z",
+          "updatedAt": "2016-06-06T05:44:44.148Z",
+          "id": "57550dcc88d11d2e3315c1df"
+        },
+        "receiver": {
+          "name": "袁毅哲",
+          "phone": "911110",
+          "address": "住在家里",
+          "createdAt": "2016-06-06T05:44:44.148Z",
+          "updatedAt": "2016-06-06T05:44:44.148Z",
+          "id": "57550dcc88d11d2e3315c1df"
+        },
+        "orderNumber": "12345672331",
+        "createdAt": "2016-06-06T06:04:02.425Z",
+        "updatedAt": "2016-06-06T06:04:02.425Z",
+        "id": "5755125264195f1238ed8743"
+      },
+      "routes": [
+        {
+          "order": "5755125264195f1238ed8743",
+          "location": {
+            "name": "中南海",
+            "createdAt": "2016-06-06T05:40:31.740Z",
+            "updatedAt": "2016-06-06T05:40:31.740Z",
+            "id": "57550ccf88d11d2e3315c1dd"
+          },
+          "context": "context1",
+          "createdAt": "2016-06-06T06:12:45.457Z",
+          "updatedAt": "2016-06-06T06:12:45.457Z",
+          "id": "5755145d7e4c3641391e36ed"
+        },
+        {
+          "order": "5755125264195f1238ed8743",
+          "location": {
+            "name": "White Palace",
+            "createdAt": "2016-06-06T06:06:00.692Z",
+            "updatedAt": "2016-06-06T06:06:00.692Z",
+            "id": "575512c864195f1238ed8744"
+          },
+          "context": "context2",
+          "createdAt": "2016-06-06T06:12:57.373Z",
+          "updatedAt": "2016-06-06T06:12:57.373Z",
+          "id": "575514697e4c3641391e36ee"
+        },
+        {
+          "order": "5755125264195f1238ed8743",
+          "location": {
+            "name": "Effel Tower",
+            "createdAt": "2016-06-06T06:06:19.027Z",
+            "updatedAt": "2016-06-06T06:06:19.027Z",
+            "id": "575512db64195f1238ed8745"
+          },
+          "context": "context3",
+          "createdAt": "2016-06-06T06:13:03.522Z",
+          "updatedAt": "2016-06-06T06:13:03.522Z",
+          "id": "5755146f7e4c3641391e36ef"
+        }
+      ]
+    }
+    res.view('test', {'order': order})
   }
+
 };
 
